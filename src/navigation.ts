307b27874f6818d9687d7bbcec1607e3724ee28e
aydocs <cihanayberk60@gmail.com>
2025-05-24 22:47:06 +0300

    updt again

--- src/navigation.ts
+++ src/navigation.ts
@@ -31,11 +31,11 @@ export const headerData = {
         },
         {
           text: 'Kullanım Koşulları',
-          href: getPermalink('/terms'),
+          href: getPermalink('/'),
         },
         {
           text: 'Gizlilik Politikası',
-          href: getPermalink('/privacy'),
+          href: getPermalink('/'),
         },
       ],
     },
@@ -63,8 +63,8 @@ export const footerData = {
         { text: 'Fiyatlandırma', href: getPermalink('/pricing') },
         { text: 'Hakkımızda', href: getPermalink('/about') },
         { text: 'İletişim', href: getPermalink('/contact') },
-        { text: 'Kullanım Koşulları', href: getPermalink('/terms') },
-        { text: 'Gizlilik Politikası', href: getPermalink('/privacy') },
+        { text: 'Kullanım Koşulları', href: getPermalink('/') },
+        { text: 'Gizlilik Politikası', href: getPermalink('/') },
       ],
     },
     {
@@ -81,18 +81,16 @@ export const footerData = {
     },
   ],
   secondaryLinks: [
-    { text: 'Kullanım Koşulları', href: getPermalink('/terms') },
-    { text: 'Gizlilik Politikası', href: getPermalink('/privacy') },
+    { text: 'Kullanım Koşulları', href: getPermalink('/') },
+    { text: 'Gizlilik Politikası', href: getPermalink('/') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://twitter.com/emadocsyazilim' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://instagram.com/emadocsyazilim' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://facebook.com/emadocsyazilim' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://onwidget.com/favicon/favicon-32x32.png" alt="onWidget logosu" loading="lazy"></img>
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://onwidget.com/"> onWidget</a> · Tüm hakları saklıdır.
+
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://emadocs.com/"> EmaDocs Yazılım</a> · Tüm hakları saklıdır.
   `,
 };
